fb81455beed70cb3f3e8765bb29e1ae72b93310d
DianaApz <kari_dia_12@hotmail.com>
2018-08-02 22:55:58 -0500

    agregan class a btn

--- src/perfil.js
+++ src/perfil.js
@@ -104,80 +104,76 @@ btnSave.addEventListener('click',()=>{
     const newPost=writeNewPost(userId,post.value);
     reload_page();
 })
+function showPublications(postContent,userId,keyPost){ 
+    let publications = document.createElement('div'); 
 
+    let sectionPost = document.createElement('textarea'); 
+    sectionPost.setAttribute('id', keyPost); 
+    sectionPost.textContent = postContent; 
 
-function showPublications(postContent,userId,keyPost){
-    let publications = document.createElement('div');
+    let sectionLike = document.createElement('div'); 
+    let pLike=document.createElement('span'); 
+    pLike.setAttribute('id', `likecount${keyPost}`);
 
-    let sectionPost = document.createElement('textarea');
-    sectionPost.setAttribute('id', keyPost);
-    sectionPost.textContent = postContent;
-
-    let sectionLike = document.createElement('div');
-    let pLike=document.createElement('span');
-
-    let btnLike= document.createElement('input');
-    btnLike.setAttribute('id', `like${keyPost}`);
-    btnLike.setAttribute('class', 'like-button');
-    btnLike.setAttribute('value','Me gusta');
-    btnLike.setAttribute('type','button');
+    let btnLike= document.createElement('input'); 
+    btnLike.setAttribute('id', `like${keyPost}`); 
+    //btnLike.setAttribute('class','falta clase'); 
+    btnLike.setAttribute('value','Me gusta'); 
+    btnLike.setAttribute('type','button'); 
 
     let sectionButtons = document.createElement('div');
 
-    let btnEdit = document.createElement('input');
-    btnEdit.setAttribute('id', `edit${keyPost}`);
-    btnEdit.setAttribute('value','Editar');
-    btnEdit.setAttribute('type','button');
-    
-    let btnDelete = document.createElement('input');
-    btnDelete.setAttribute('id', `delete${keyPost}`);
-    btnDelete.setAttribute('value','Eliminar');
-    btnDelete.setAttribute('type','button');
-    
-    sectionLike.appendChild(btnLike);
-    sectionLike.appendChild(pLike);
+    let btnEdit = document.createElement('input'); 
+    btnEdit.setAttribute('id', `edit${keyPost}`); 
+    //btnEdit.setAttribute('class','falta clase'); 
+    btnEdit.setAttribute('value','Editar'); 
+    btnEdit.setAttribute('type','button'); 
 
-    sectionButtons.appendChild(btnDelete);
-    sectionButtons.appendChild(btnEdit);
-    
-    posted.appendChild(publications);
-    publications.appendChild(sectionPost);
-    publications.appendChild(sectionLike);
+    let btnDelete = document.createElement('input'); 
+    btnDelete.setAttribute('id', `delet${keyPost}`); 
+    //btnDelete.setAttribute('class','falta clase'); 
+    btnDelete.setAttribute('value','Eliminar'); 
+    btnDelete.setAttribute('type','button'); 
+
+    sectionLike.appendChild(btnLike); 
+    sectionLike.appendChild(pLike); 
+    sectionButtons.appendChild(btnDelete); 
+    sectionButtons.appendChild(btnEdit); 
+    posted.appendChild(publications); 
+    publications.appendChild(sectionPost); 
+    publications.appendChild(sectionLike); 
     publications.appendChild(sectionButtons);
-    
-    let likes = document.getElementById(`like${keyPost}`);
-    let edit = document.getElementById(`edit${keyPost}`);
-    let delet = document.getElementById(`delete${keyPost}`);
-    
 
+    let likePoints = document.querySelector(`#likecount${keyPost}`); 
 
-    likes.addEventListener('click', () => {
-        let userId = firebase.auth().currentUser.uid;
-        //const currentPost = document.getElementById(`like${keyPost}`);
-        const likeButton = likes.querySelector('.like-button');
+    let likes = document.querySelector(`#like${keyPost}`); 
+    likes.addEventListener('click', () => { 
+        let userId = firebase.auth().currentUser.uid; 
+        //const currentPost = document.getElementById(`like${keyPost}`); 
+        const likeButton = likes.querySelector('.like-button'); 
         firebase.database().ref('posts/' + keyPost) 
-        .once('value', (postRef) =>{
-          const postLike = postRef.val();
-          
-          const objRefLike = postLike.postWithLikes || [];
-          
-          if (objRefLike.indexOf(userId) === -1) {
-            objRefLike.push(userId);
-            postLike.likeCount = objRefLike.length;
-          } else if (objRefLike.indexOf(userId) === 0) {
-            likeButton.disabled = false;
-          }
-          postLike.postWithLikes = objRefLike;
-      
-          let updates = {};
-          updates['/posts/' + keyPost] = postLike;
-          updates['/user-posts/' + userId + '/' + keyPost] = postLike;
-          return firebase.database().ref().update(updates);
-          
-        })
-      
+        .once('value', (postRef) =>{ 
+            const postLike = postRef.val(); 
+
+            const objRefLike = postLike.postWithLikes || []; 
+
+            likePoints.innerHTML=objRefLike.length; 
+            if (objRefLike.indexOf(userId) === -1) { 
+                objRefLike.push(userId); 
+                postLike.likeCount = objRefLike.length; 
+            } else if (objRefLike.indexOf(userId) === 0) { 
+                likeButton.disabled = false; 
+            }
+            postLike.postWithLikes = objRefLike; 
+
+            let updates = {}; 
+            updates['/posts/' + keyPost] = postLike; 
+            updates['/user-posts/' + userId + '/' + keyPost] = postLike; 
+            return firebase.database().ref().update(updates); 
+        }) 
     });
-    
+
+    let delet = document.querySelector(`#delet${keyPost}`);
     delet.addEventListener('click', () => {
         firebase.database().ref().child('/user-posts/' + userId + '/' + keyPost).remove();
         firebase.database().ref().child('posts/' + keyPost).remove();
@@ -185,6 +181,8 @@ function showPublications(postContent,userId,keyPost){
         while(posted.firstChild) posted.removeChild(posted.firstChild);
         reload_page()
     });
+
+    let edit = document.querySelector(`#edit${keyPost}`);
     edit.addEventListener('click', () => {
         const editPost = document.getElementById(keyPost);
       const nuevoPost = {
@@ -203,7 +201,7 @@ function showPublications(postContent,userId,keyPost){
         
     
 }
-    
+   
 function reload_page() {
     window.location.reload();
 }; 
